31977623ecbf4943cb374d602187f12e7f92d483
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-09-11 23:41:33 -0400

    good night

--- docs/code/orchard.R
+++ docs/code/orchard.R
@@ -1,5 +1,5 @@
-
 library(ggplot2); theme_set(theme_bw(base_size=18))
+library(magrittr)
 library(shellpipes); startGraphics()
 
 library(Hmisc)
@@ -9,39 +9,39 @@ base <- ggplot(OrchardSprays,aes(x=treatment,y=decrease))
 lbase <- base +  scale_y_log10()
 
 ## Bar charts, lin and log
-print(bar <- base
+(bar <- base
     +  stat_summary(fun.data=mean_cl_normal,geom="bar",colour="gray")
     +  stat_summary(fun.data=mean_cl_normal,geom="errorbar",width=0.5)
-)
+) %>% teeGG(desc="bar")
 
-print(bar +  scale_y_log10())
+(bar +  scale_y_log10() + ggtitle("Aphids!")) %>% teeGG(desc="logbar")
 
 ## pointrange is similar to error bar; these are are the non-dynamite analogues of the above
-print(base 
+(base 
 	+ stat_summary(fun.data=mean_cl_normal,geom="pointrange")
 )
 
-print(lbase 
+(lbase 
 	+ stat_summary(fun.data=mean_cl_normal,geom="pointrange")
 )
 
 ## sd (default is 2 sds!!)
-print(lsd <- lbase 
+(lsd <- lbase 
 	+ stat_summary(fun.data=mean_sdl
 		, geom="pointrange"
 		, fun.args = list(mult=1)
 	)
 )
 
-print(base 
+(base 
 	+ stat_summary(fun.data=mean_sdl
 		, geom="pointrange"
 		, fun.args = list(mult=1)
 	)
 )
 
-print(lbase + geom_point())
-print(lbase + geom_boxplot())
-print(lbase + geom_violin())
+(lbase + geom_point())
+(lbase + geom_boxplot())
+(lbase + geom_violin())
 
-print(lsd + geom_point(color="blue", alpha=0.3))
+(lsd + geom_point(color="blue", alpha=0.3))
