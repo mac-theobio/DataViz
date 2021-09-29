cf2237ecad1dab00820b77c899b178dafef75d07
dushoff <dushoff@mcmaster.ca>
2021-09-29 14:21:51 -0400

    Live push!

--- docs/code/lap_plots.R
+++ docs/code/lap_plots.R
@@ -1,5 +1,9 @@
-library(ggplot2)
+library(ggplot2); theme_set(theme_classic(base_size=12))
+library(dplyr)
+
 library(shellpipes)
+
+rpcall("vlap_plots.Rout lap_plots.R data/vlaps.rds")
 
 lap <- rdsRead()
 
@@ -10,4 +14,18 @@ print(ggplot(lap)
 	+ geom_bar()
 	+ coord_flip()
 )
+
+print(ggplot(lap)
+	+ aes(x=circuit)
+	+ geom_bar()
+	+ coord_flip()
+)
+
+print(time1 <- ggplot(lap)
+	+ aes(x=time)
+	+ geom_histogram()
+)
+
+## %+% here is some new data keep everything else the same
+print(time1 %+% (lap %>% filter(time<300)))
 
--- docs/code/vlaps.R
+++ docs/code/vlaps.R
@@ -2,6 +2,8 @@ library(shellpipes)
 library(dplyr)
 library(forcats)
 
+rpcall("vlaps.Rout vlaps.R f1data/drivers.csv f1data/races.csv f1data/lap_times.csv f1data/circuits.csv f1data")
+
 vlap <- (csvRead("drivers")
 	%>% filter(surname=="Verstappen")
 	%>% left_join(csvRead("times"))
@@ -12,6 +14,8 @@ vlap <- (csvRead("drivers")
 		, race=name.x, circuit=name.y
 		, year, lap, position, time=milliseconds/1000
 	)
+	%>% mutate(across(where(is.character), ~fct_rev(fct_infreq(.))))
+	## %>% mutate(race=fct_rev(fct_infreq(race)))
 )
 csvSave(vlap)
 rdsSave(vlap)
