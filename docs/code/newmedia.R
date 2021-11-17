5b327825118dddb572ac1fba13f6ffdab42ec660
dushoff <dushoff@mcmaster.ca>
2021-11-17 18:24:07 -0500

    lecture changes

--- docs/code/newmedia.R
+++ docs/code/newmedia.R
@@ -9,31 +9,29 @@ etmp <- read.csv("data/sunbelt/Dataset1-Media-Example-EDGES.csv", header=TRUE, a
 
 ## summary(etmp)
 
+## use dplyr-like stuff with the tbl_graph using activate()
 citen <- tbl_graph(ntmp, etmp)
 
 summary(citen)
 print(citen)
 
-hb <- ggraph(citen, layout="kk")
+hb <- ggraph(citen)
 
-print(hb 
-	+ geom_edge_link()
-	+ geom_node_point()
-	+ geom_node_text(aes(label=id), repel=TRUE)
-)
-
-quit()
+## wran <- range(activate …
 
 print(hb 
 	+ geom_edge_fan(aes(color=type, width=weight)
 		, arrow = arrow(length = unit(3, 'mm'))
 		, end_cap = circle(1.5, 'mm')
-		, alpha=0.3
+		, alpha=0.4
 	) + scale_edge_width(range=c(0.1, 2.3))
 	+ geom_node_point(aes(size=audience.size, color=type.label))
+	+ scale_color_brewer(palette="Dark2")
 	+ scale_size_area(max_size=3)
 	+ geom_node_text(aes(label=media), repel=TRUE)
 )
+
+quit()
 
 print(ggraph(citen, layout="hive", axis=type.label, sort.by=-audience.size)
 	+ geom_edge_hive()
