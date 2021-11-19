5772c85d9d77ef4100685844bf67ea10e0336241
dushoff <dushoff@mcmaster.ca>
2021-11-19 10:33:00 -0500

    edge colors

--- docs/code/newmedia.R
+++ docs/code/newmedia.R
@@ -23,18 +23,25 @@ print(hb
 	+ geom_edge_fan(aes(color=type, width=weight)
 		, arrow = arrow(length = unit(3, 'mm'))
 		, end_cap = circle(1.5, 'mm')
-		, alpha=0.4
+		, alpha=0.8
 	) + scale_edge_width(range=c(0.1, 2.3))
 	+ geom_node_point(aes(size=audience.size, color=type.label))
-	+ scale_color_brewer(palette="Dark2")
+	+ scale_color_discrete()
+	+ scale_edge_color_discrete()
 	+ scale_size_area(max_size=3)
 	+ geom_node_text(aes(label=media), repel=TRUE)
 )
 
-quit()
+## Hive plot -- good for cross-category links only
+## Also, seems like a mess
 
 print(ggraph(citen, layout="hive", axis=type.label, sort.by=-audience.size)
-	+ geom_edge_hive()
-	+ geom_node_point()
+	+ geom_edge_hive(aes(color=type, width=weight)
+		, arrow = arrow(length = unit(3, 'mm'))
+		, end_cap = circle(1.5, 'mm')
+		, alpha=0.4
+	)
+	+ geom_node_point(aes(size=audience.size, color=type.label))
 	+ geom_axis_hive(aes(color=type.label), label=FALSE)
+	+ geom_node_text(aes(label=media), repel=TRUE)
 )
