ce5caec7c4bddb858bd9342ff67d2e2427509fc0
dushoff <dushoff@mcmaster.ca>
2021-11-19 11:15:22 -0500

    fiddle with hive and colors

--- docs/code/newmedia.R
+++ docs/code/newmedia.R
@@ -1,6 +1,7 @@
 library(tidygraph)
 library(igraph)
 library(ggraph); theme_set(theme_void())
+library(dplyr)
 
 set.seed(213)
 
@@ -10,7 +11,12 @@ etmp <- read.csv("data/sunbelt/Dataset1-Media-Example-EDGES.csv", header=TRUE, a
 ## summary(etmp)
 
 ## use dplyr-like stuff with the tbl_graph using activate()
-citen <- tbl_graph(ntmp, etmp)
+citen <- (tbl_graph(ntmp, etmp)
+	%>% activate("edges")
+	%>% rename(`link type`=type)
+	%>% activate("nodes")
+	%>% rename(medium=type.label)
+)
 
 summary(citen)
 print(citen)
@@ -20,30 +26,36 @@ hb <- ggraph(citen)
 ## wran <- range(activate …
 
 print(hb 
-	+ geom_edge_fan(aes(color=type, width=weight)
+	+ geom_edge_fan(aes(color=`link type`, width=weight)
 		, arrow = arrow(length = unit(3, 'mm'))
 		, end_cap = circle(1.5, 'mm')
 		, alpha=0.8
-	) + scale_edge_width(range=c(0.1, 2.3))
-	+ geom_node_point(aes(size=audience.size, color=type.label))
+	) 
+	+ scale_edge_width(range=c(0.1, 2.3))
+	+ guides(edge_width="none")
+	+ geom_node_point(aes(size=audience.size, color=medium))
+	+ guides(size="none")
 	+ scale_color_discrete(l=40)
-	+ scale_edge_color_discrete(l=90)
+	+ scale_edge_color_discrete(l=90, c=70)
 	+ scale_size_area(max_size=3)
 	+ geom_node_text(aes(label=media), repel=TRUE)
 )
 
 ## Hive plot -- good for cross-category links only
-## Also, seems like a mess
 
-print(ggraph(citen, layout="hive", axis=type.label, sort.by=-audience.size)
-	+ geom_edge_hive(aes(color=type, width=weight)
+print(ggraph(citen, layout="hive", axis=medium)
+	+ geom_edge_hive(aes(color=`link type`, width=weight)
 		, arrow = arrow(length = unit(3, 'mm'))
 		, end_cap = circle(1.5, 'mm')
-		, alpha=0.4
-	)
-	+ geom_node_point(aes(size=audience.size, color=type.label))
-	+ geom_axis_hive(aes(color=type.label), label=FALSE)
-	+ geom_node_text(aes(label=media), repel=TRUE)
+		, alpha=0.8
+	) 
+	+ scale_edge_width(range=c(0.1, 2.3))
+	+ guides(edge_width="none")
+	+ geom_node_point(aes(size=audience.size, color=medium))
+	+ guides(size="none")
 	+ scale_color_discrete(l=40)
-	+ scale_edge_color_discrete(l=90)
+	+ scale_edge_color_discrete(l=90, c=70)
+	+ scale_size_area(max_size=3)
+	+ geom_node_text(aes(label=media), repel=TRUE)
+	+ geom_axis_hive(aes(color=medium), label=FALSE)
 )
